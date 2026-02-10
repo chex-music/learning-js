a10fc9b6b6f241c4742aefddc6b4fa7c30debdb0
chex-music <desaichaitanya012@gmail.com>
2026-02-10 10:48:56 +0000

    comparisons

---
+++ 01_BASICS/04_comparision.js
@@ -0,0 +1,13 @@
+// console.log("2" > 1);//ture
+// console.log("2" < 1);//false
+
+// console.log(null > 0); // null converted into 0 and then it compared by 0
+// console.log(null == 0);
+
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+
+//strict checks
+console.log("2" === 2);
+
