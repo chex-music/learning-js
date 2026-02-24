e58c7909078b9860c8cc3080309c61ff82270aa0
chex-music <desaichaitanya012@gmail.com>
2026-02-24 06:08:28 +0000

    Refactor IIFE examples to enhance clarity and consistency

--- 03_BASICS/04_iife.js
+++ 03_BASICS/04_iife.js
@@ -1,9 +1,19 @@
+
+// pronoounce :iffy
+//unnamed IIFE
 (
     function() {
         console.log('This is an IIFE (Immediately Invoked Function Expression)');
     }
 )();
-
+//named IIFE with expression
 let result = (function dbConnect() {
         console.log('Connecting to the database...');
 })(); // It will print Connecting to the database... because the function dbConnect is an IIFE (Immediately Invoked Function Expression) which means it will be executed immediately after it is defined.
+
+//unnamed IIFE using arrow function
+(
+    (name)=>{
+        console.log(`This is an arrow function ${name} `);
+    }
+)('IIFE');
