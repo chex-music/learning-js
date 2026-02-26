c46b8380f98c5688315ff503d538b831fdef8b49
chex-music <desaichaitanya012@gmail.com>
2026-02-26 09:24:54 +0000

    reduce function implimentation

--- map_and_reduce.js
+++ map_and_reduce.js
@@ -23,7 +23,7 @@ const numbers = [1, 2, 3, 4, 5];
 // });
 // console.log(evenNumbers); 
 
-//
+// //
 const books = [
     { title: "Book A", price: 10,genre:"Fiction" },
     { title: "Book B", price: 15,genre:"Non-Fiction" },
@@ -31,8 +31,28 @@ const books = [
     { title: "Book D", price: 25,genre:"Science Fiction" },
 ];
 
-const result = books.filter((book) => {
-    return book.genre === "Fiction";
-});
+// const result = books.filter((book) => {
+//     return book.genre === "Fiction";
+// });
 
-console.log(result);
+// console.log(result);
+// const newnumbers = numbers.filter((num) => num * 2);
+// console.log(newnumbers); // outputs [1, 2, 3, 4, 5] because filter does not modify the original array and the condition is not filtering any values
+
+// const doubledNumbers = numbers.map((num) => num * 2);
+// console.log(doubledNumbers); // outputs [2, 4, 6, 8, 10] because map returns a new array with the modified values
+// console.log(numbers);// outputs [1, 2, 3, 4, 5] because map does not modify the original array
+
+//chaining means using multiple array methods together in a single expression. 
+// For example, we can use filter and map together to first filter out even numbers and then double them:
+// const newnumbers2 = numbers
+//                     .filter((num) => num % 2 === 0)
+//                     .map((num) => num * 2);
+// console.log(newnumbers2); // outputs [4, 8] because we first filter out even numbers (2 and 4) and then double them (4 and 8)
+
+// The reduce method is used to reduce an array to a single value by applying a function to each element of the array.
+const myTotal = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);// the second argument (0) is the initial value of the accumulator
+console.log(myTotal);
+
+// const totalPrice = books.reduce((total, book) => total + book.price, 0);
+// console.log(totalPrice);
